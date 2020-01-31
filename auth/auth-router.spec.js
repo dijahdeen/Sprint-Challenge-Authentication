c02df5deb9918e240f29c5bc62681a758d6dcdef
Dijah Deen <dijahdeen@Dijahs-MacBook-Air.local>
2020-01-31 13:01:57 -0500

    testing completed

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,11 +1,20 @@
 const server = require('../api/server');
-const supereq = require('supertest');
+const requires = require('supertest');
 
-describe ('users', () =>{
+describe ('auth router', () =>{
     it('post /register', async() => {
-        const res = await supereq(server)
+        const res = await requires(server)
                 .post('/register')
-                .send({username:'dijah'})
-            expect(res.status).toBe (200);
+                .send({username:'dijah', password: '1234'})
+                .then(res => {
+            expect(res.status).toBe(404);
+        });
+    });
+
+    it('post /login', async() => {
+        const res = await requires(server)
+                .post('/login')
+                .send({username:'lambda', password:"idkyet"})
+            expect(res.status)
     });
 });
